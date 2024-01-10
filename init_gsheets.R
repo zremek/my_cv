160469611c304ea4a7f1b70517b415e0fb329383
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2024-01-10 17:42:01 +0100

    test

---
+++ init_gsheets.R
@@ -0,0 +1,3 @@
+library(googlesheets4)
+
+googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/13uQcsdVCP2Mes7t-Xl4--kYbpbm0a0_H2PKz4aW1nAU/edit#gid=917338460")
---
+++ make_cv.R
@@ -0,0 +1,37 @@
+# https://github.com/nstrayer/datadrivencv/issues/81#issuecomment-1014809230 
+
+# remotes::install_local("datadrivencv-master.zip")
+# 
+# remotes::install_github("mitchelloharawild/icons")
+# 
+# library(icons)
+# 
+
+# run ?datadrivencv::use_datadriven_cv to see more details
+datadrivencv::use_datadriven_cv(
+  full_name = "Remigiusz Żulicki",
+  data_location = "https://docs.google.com/spreadsheets/d/13uQcsdVCP2Mes7t-Xl4--kYbpbm0a0_H2PKz4aW1nAU/edit?usp=sharing",
+  pdf_location = "https://github.com/zremek/my_cv/raw/master/zremek_cv.pdf",
+  html_location = "https://github.com/zremek/my_cv/raw/master/zremek_cv.html",
+  source_location = "https://github.com/zremek/my_cv"
+)
+
+
+# Knit the HTML version
+rmarkdown::render("cv.rmd",
+                  params = list(pdf_mode = FALSE),
+                  output_file = "cv.html")
+
+# Knit the PDF version to temporary html location
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("cv.rmd",
+                  params = list(pdf_mode = TRUE),
+                  output_file = tmp_html_cv_loc)
+
+# Convert to PDF using Pagedown
+pagedown::chrome_print(input = tmp_html_cv_loc,
+                       output = "cv.pdf")
+
+
+
+
